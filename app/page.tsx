cf597ccafa4f66c76617a7f707b07ec52711b88b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-16 09:09:56 +0530

    🎯project-environment-setup🛠️

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-gray-500 font-bold uppercase">
+        The Continental Hotel
+      </h1>
+    </div>
+  );
+}
