41a22b1726418b2f58e20d7a3666d76f6738cddb
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-16 09:21:51 +0530

    🛠️installing-and-configuring-ui-library (ant design) 🎨

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "antd";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl text-gray-500 font-bold uppercase">
         The Continental Hotel
       </h1>
+      <Button type="primary">Confirm your booking!</Button>
     </div>
   );
 }
