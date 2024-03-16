ccbb496e0929fecd7b4338b1cb491e43597d01e2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-16 09:40:40 +0530

    ant-design-theming

---
+++ components/providers/theme-provider.tsx
@@ -0,0 +1,34 @@
+import { ConfigProvider } from "antd";
+import React from "react";
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#000",
+            borderRadius: 3,
+          },
+          components: {
+            Button: {
+              controlHeight: 42,
+              defaultBorderColor: "#000",
+            },
+            Input: {
+              controlHeight: 42,
+              activeShadow: "none",
+              boxShadow: "none",
+              borderRadius: 3,
+              colorBorder: "#ccc",
+            },
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </div>
+  );
+};
+
+export default ThemeProvider;
