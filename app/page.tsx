d077f68dd34f6f9762117bd6d55f46c5aae9b0a6
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-18 09:42:12 +0530

    🔗connect-to-mongoDB

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { connectToDatabase } from "@/lib/db";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "antd";
 
+connectToDatabase();
+
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-5 p-5 items-center justify-center h-screen">
       <h1 className="text-3xl text-gray-500 font-bold uppercase">
         The Continental Hotel
       </h1>
