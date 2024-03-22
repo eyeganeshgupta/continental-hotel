917f3519ef36e13319db6bc1586dafbad4167ffd
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-22 09:42:50 +0530

    sidebar⚛️

--- components/providers/layout-provider/Sidebar.tsx
+++ components/providers/layout-provider/Sidebar.tsx
@@ -1,5 +1,80 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import { Drawer } from "antd";
+import { BedDouble, GitGraph, Home, Hotel, List, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const Sidebar = ({ showSidebar, setShowSidebar, user }: any) => {
+  const router = useRouter();
+
+  const iconSize = 18;
+
+  const userMenuItems: any[] = [
+    {
+      name: "Home",
+      icon: <Home size={iconSize} />,
+      onClick: () => router.push("/"),
+    },
+    {
+      name: "Bookings",
+      icon: <List size={iconSize} />,
+      onClick: () => router.push("/user/bookings"),
+    },
+    {
+      name: "Profile",
+      icon: <User size={iconSize} />,
+      onClick: () => router.push("/user/profile"),
+    },
+  ];
+  const adminMenuItems: any[] = [
+    {
+      name: "Home",
+      icon: <Home size={iconSize} />,
+      onClick: () => router.push("/"),
+    },
+    {
+      name: "Bookings",
+      icon: <List size={iconSize} />,
+      onClick: () => router.push("/admin/bookings"),
+    },
+    {
+      name: "Hotels",
+      icon: <Hotel size={iconSize} />,
+      onClick: () => router.push("/admin/hotels"),
+    },
+    {
+      name: "Rooms",
+      icon: <BedDouble size={iconSize} />,
+      onClick: () => router.push("/admin/rooms"),
+    },
+    {
+      name: "Reports",
+      icon: <GitGraph size={iconSize} />,
+      onClick: () => router.push("/admin/reports"),
+    },
+  ];
+
+  const menuItemsToShow: any[] = user?.isAdmin ? adminMenuItems : userMenuItems;
+
+  return (
+    <Drawer open={showSidebar} onClose={() => setShowSidebar(false)} closable>
+      <div className="flex flex-col gap-14">
+        {menuItemsToShow.map((item, index) => {
+          return (
+            <div
+              className="flex items-center gap-4 text-gray-700 cursor-pointer"
+              key={item.name + " " + index}
+              onClick={() => {
+                item.onClick();
+                setShowSidebar(false);
+              }}
+            >
+              {item.icon}
+              <span className="mt-[2px]">{item.name}</span>
+            </div>
+          );
+        })}
+      </div>
+    </Drawer>
+  );
 };
 
 export default Sidebar;
--- components/providers/layout-provider/UserInfo.tsx
+++ components/providers/layout-provider/UserInfo.tsx
@@ -1,10 +1,24 @@
 import { User } from "lucide-react";
+import { useState } from "react";
+import Sidebar from "./Sidebar";
 
 const UserInfo = ({ user }: any) => {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className="p-5 border-0 border-l border-solid flex items-center gap-5">
       <span className="text-gray-500 text-sm">{user.name}</span>
-      <User className="text-gray-500" />
+      <User
+        className="text-gray-500"
+        onClick={() => setShowSidebar(!showSidebar)}
+      />
+
+      {showSidebar && (
+        <Sidebar
+          showSidebar={showSidebar}
+          setShowSidebar={setShowSidebar}
+          user={user}
+        />
+      )}
     </div>
   );
 };
