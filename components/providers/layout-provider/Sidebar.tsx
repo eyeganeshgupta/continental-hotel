1b9c64876b391ebaabf57759c8c5ef16a5759da0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-22 09:54:00 +0530

    sidebar-active-menu ☰

--- components/providers/layout-provider/Sidebar.tsx
+++ components/providers/layout-provider/Sidebar.tsx
@@ -1,9 +1,10 @@
 import { Drawer } from "antd";
 import { BedDouble, GitGraph, Home, Hotel, List, User } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Sidebar = ({ showSidebar, setShowSidebar, user }: any) => {
   const router = useRouter();
+  const pathname = usePathname();
 
   const iconSize = 18;
 
@@ -12,43 +13,52 @@ const Sidebar = ({ showSidebar, setShowSidebar, user }: any) => {
       name: "Home",
       icon: <Home size={iconSize} />,
       onClick: () => router.push("/"),
+      isActive: pathname === "/",
     },
     {
       name: "Bookings",
       icon: <List size={iconSize} />,
       onClick: () => router.push("/user/bookings"),
+      isActive: pathname === "/user/bookings",
     },
     {
       name: "Profile",
       icon: <User size={iconSize} />,
       onClick: () => router.push("/user/profile"),
+      isActive: pathname === "/user/profile",
     },
   ];
+
   const adminMenuItems: any[] = [
     {
       name: "Home",
       icon: <Home size={iconSize} />,
       onClick: () => router.push("/"),
+      isActive: pathname === "/",
     },
     {
       name: "Bookings",
       icon: <List size={iconSize} />,
       onClick: () => router.push("/admin/bookings"),
+      isActive: pathname === "/admin/bookings",
     },
     {
       name: "Hotels",
       icon: <Hotel size={iconSize} />,
       onClick: () => router.push("/admin/hotels"),
+      isActive: pathname === "/admin/hotels",
     },
     {
       name: "Rooms",
       icon: <BedDouble size={iconSize} />,
       onClick: () => router.push("/admin/rooms"),
+      isActive: pathname === "/admin/rooms",
     },
     {
       name: "Reports",
       icon: <GitGraph size={iconSize} />,
       onClick: () => router.push("/admin/reports"),
+      isActive: pathname === "/admin/reports",
     },
   ];
 
@@ -60,7 +70,9 @@ const Sidebar = ({ showSidebar, setShowSidebar, user }: any) => {
         {menuItemsToShow.map((item, index) => {
           return (
             <div
-              className="flex items-center gap-4 text-gray-700 cursor-pointer"
+              className={`flex items-center gap-4 text-gray-700 cursor-pointer px-7 py-3 rounded ${
+                item.isActive ? "bg-gray-700 text-white" : ""
+              }`}
               key={item.name + " " + index}
               onClick={() => {
                 item.onClick();
