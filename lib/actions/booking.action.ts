bf53cdf62ec7859314c7ee3cccd66d2f2cbb1794
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-04-01 09:21:45 +0530

    check-room-availability📅

---
+++ lib/actions/booking.action.ts
@@ -0,0 +1,56 @@
+import Booking from "@/database/booking.model";
+import { connectToDatabase } from "../db";
+
+export const checkRoomAvailability = async ({
+  roomId,
+  reqCheckInDate,
+  reqCheckOutDate,
+}: {
+  roomId: string;
+  reqCheckInDate: string;
+  reqCheckOutDate: string;
+}) => {
+  try {
+    connectToDatabase();
+
+    const bookedSlot = await Booking.findOne({
+      room: roomId,
+      bookingStatus: "Booked",
+      $or: [
+        {
+          checkInDate: {
+            $gte: reqCheckInDate,
+            $lte: reqCheckOutDate,
+          },
+        },
+        {
+          checkOutDate: {
+            $gte: reqCheckInDate,
+            $lte: reqCheckOutDate,
+          },
+        },
+        {
+          $and: [
+            { checkInDate: { $lte: reqCheckInDate } },
+            { checkOutDate: { $gte: reqCheckOutDate } },
+          ],
+        },
+      ],
+    });
+
+    if (bookedSlot) {
+      return {
+        success: false,
+      };
+    }
+
+    return {
+      success: true,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
